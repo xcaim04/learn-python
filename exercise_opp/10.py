19e0695d16b9149af70b062c809262e2570e87d6
Carlos Blanco <carlojbm04@gmail.com>
2024-05-12 17:10:50 +0200

    Test exercise 10 in exercise_opp

--- exercise_opp/10.py
+++ exercise_opp/10.py
@@ -1,3 +1,4 @@
+import unittest
 class Estudiante:
 
     school = 'MIT'
@@ -6,12 +7,14 @@ class Estudiante:
         self.name = name
 
     def _student_attendance(self, attendance) -> int:
+        res = 3
         for mes, attendance in attendance.items():
             if attendance < 4:
-                return 1
+                res = min(res, 1)
             elif 4 <= attendance < 8:
-                return 2
-            return 3
+                res = min(res, 2)
+            else: res = min(res, 3)
+        return res
 
     # Encapsulacion del metodo privado
     def get_students_attendances(self, list_subject_attendances) -> int:
@@ -31,11 +34,38 @@ class Estudiante:
         self.grade = grade
         self.media = sum(list_notes) / len(list_notes)
 
+student = Estudiante('Carlos')
 
-if __name__ ==  '__main__':
+def test_1():
+    attendance = {"Enero": 6, "Febrero": 3, "Marzo": 8}
+    test = student.get_students_attendances(attendance)
+    print(attendance, 'Value expected: 1 ', f'Value received: {test}')
+    try:
+        assert test == 1
+        print('\t> Test 1: Accepted\n')
+    except:
+        print('\t> Test 1: Wrong Answer\n')
 
-    student = Estudiante("John")
-    asistencias1 = {"Enero": 6, "Febrero": 3, "Marzo": 8}
+def test_2():
+    attendance = {"Enero": 6, "Febrero": 10, "Marzo": 8}
+    test = student.get_students_attendances(attendance)
+    print(attendance, 'Value expected: 2 ', f'Value received: {test}')
+    try:
+        assert test == 2
+        print('\t> Test 2: Accepted\n')
+    except:
+        print('\t> Test 2: Wrong Answer\n')
 
-    attendance = student.get_students_attendances(asistencias1)
-    print(attendance)
+def test_3():
+    attendance = {"Enero": 60, "Febrero": 10, "Marzo": 18}
+    test = student.get_students_attendances(attendance)
+    print(attendance, 'Value expected: 1 ', f'Value received: {test}')
+    try:
+        assert test == 3
+        print('\t> Test 3: Accepted\n')
+    except:
+        print('\t> Test 3: Wrong Answer\n')
+
+test_1()
+test_2()
+test_3()
