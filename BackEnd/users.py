706e27a351bd5d9f18183fd37dfaecf147171409
Carlos Blanco <carlojbm04@gmail.com>
2024-07-02 15:41:52 +0200

    Query user on the user's list

--- BackEnd/users.py
+++ BackEnd/users.py
@@ -1,18 +1,38 @@
 from fastapi import FastAPI
 from pydantic import BaseModel # Definir una entidad users
 
-
-
 app = FastAPI()
 
-@app.get('/users')
-async def users():
-    return [
-        {
-            'name': 'Carlos',
-            'surname': 'Blanco'
-        },
-        {
-            'name': 'Javier',
-            'surname': 'Blanco'
-        },]
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    age: int
+    url: str
+
+userlist = [User(id=4, name='Carlos', surname='Javier', age=19, url='https://carlosblanco.dev'),
+        User(id=44 ,name='Javier', surname='Andres', age=10, url='https://javierblanco.dev')]
+
+
+""" Search a user by an id on userlist[] """
+
+def search_user(ID: int) -> User:
+    my_users = filter(lambda user: user.id == ID, userlist)
+    
+    try:
+        return list(my_users)[0]
+    except:
+        return {'error': 'Not found 404'}
+
+
+
+# Query => url.../?user_id=1/
+@app.get("/user")
+async def user(user_id: int):
+    return search_user(user_id)
+
+
+# Path => url.../userquery/4
+@app.get("/userquery/{user_id}")
+async def user(user_id: int):
+    return search_user(user_id)
